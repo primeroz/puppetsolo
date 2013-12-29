730af42126ffb0bb163709b83a4b4300143f89a0
Francesco Ciocchetti <primeroznl@gmail.com>
2013-12-29 18:38:52 +0000

    Parameterize basedir and test out some stuff for puppet apply

--- modules/base/manifests/basedir.pp
+++ modules/base/manifests/basedir.pp
@@ -1,10 +1,24 @@
-class base::basedir {
+class base::basedir (
+  config = undef
+) {
 
-    file {  '/data':
-                ensure =>   "directory",
-                owner  =>   "root",
-                group  =>   "root",
-                mode   =>   "0755",
-    }
+  $defaults = {
+    ensure => 'directory'
+    owner  => 'root'
+    group  => 'root'
+  }
+
+  # Create Dirs from config argument hash
+  if $config {
+    validate_hash("$config")
+    create_resources(file,$config,$defaults)
+  }
+
+  # Pull daya from HIERA as merged hash
+  $hiera_config = hiera_hash('base::basedir::basedirs', undef)
+  if $hiera_config {
+    validate_hash("$hiera_config")
+    create_resources(file,$hiera_config,$defaults)
+  }
 
 }
