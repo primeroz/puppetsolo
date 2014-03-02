16e08b0d78e51ca38f0b7cdbd75269cdcd7d9c5d
Francesco Ciocchetti <primeroznl@gmail.com>
2014-03-02 12:18:23 +0000

    Make Debian safe

--- modules/base/manifests/basepkgs.pp
+++ modules/base/manifests/basepkgs.pp
@@ -5,35 +5,45 @@ class base::basepkgs (
 
   $hiera_config_common = hiera('base::basepkgs::common', undef)
 
-  case $::operatingsystemmajrelease {
-    6: { 
-      $hiera_config_release = hiera('base::basepkgs::6', undef)
-      $epel_vers = "epel-6"
-    }
-    5: { 
-      $hiera_config_release = hiera('base::basepkgs::5', undef)
-      $epel_vers = "epel-5"
-    }
-  }
+  case $::osfamily {
 
-  # Enable EPEL repo my mirrors
-  yumrepo {
-      "epel":
-          descr          =>   "Epel Repository for CentOS",
-          mirrorlist     =>   "http://mirrors.fedoraproject.org/mirrorlist?repo=${epel_vers}&arch=\$basearch",
-          failovermethod =>   "priority",
-          gpgcheck       =>   "0",
-          enabled        =>   "1";
-  }
+    'Redhat': {
 
-  package { $hiera_config_common:
-              ensure  => 'installed',
-              require => Yumrepo['epel']
-  }
+      case $::operatingsystemmajrelease {
+        6: { 
+          $hiera_config_release = hiera('base::basepkgs::6', undef)
+          $epel_vers = "epel-6"
+        }
+        5: { 
+          $hiera_config_release = hiera('base::basepkgs::5', undef)
+          $epel_vers = "epel-5"
+        }
+      }
 
-  package { $hiera_config_release:
-              ensure  => 'installed',
-              require => Yumrepo['epel']
+      # Enable EPEL repo my mirrors
+      yumrepo {
+          "epel":
+              descr          =>   "Epel Repository for CentOS",
+              mirrorlist     =>   "http://mirrors.fedoraproject.org/mirrorlist?repo=${epel_vers}&arch=\$basearch",
+              failovermethod =>   "priority",
+              gpgcheck       =>   "0",
+              enabled        =>   "1";
+      }
+
+      package { $hiera_config_common:
+                  ensure  => 'installed',
+                  require => Yumrepo['epel']
+      }
+
+      package { $hiera_config_release:
+                  ensure  => 'installed',
+                  require => Yumrepo['epel']
+      }
+
+    } # End Redhat
+    'Debian': {
+    } # End Debian
+
   }
 
 }
